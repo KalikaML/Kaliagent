73932f44ab8e262b6b6978e190c88beee6b1bb16
Yuvraj2110068 <yuvrajput1537@gmail.com>
2025-08-08 13:13:50 +0530

    Update procurement folder and modify urls

--- AI_agent_center/command_center/urls.py
+++ AI_agent_center/command_center/urls.py
@@ -1,19 +1,17 @@
 # command_center/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
-from django.urls import path
-
-
-app_name = 'ai_agent_pitch'
+from django.conf import settings  # <-- ADD THIS LINE
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('marketing/', include('marketing_outreach.urls')),
-    path('procurement/', include('procurement.urls')),
     path('pitch/', include('ai_agent_pitch.urls')),
     path('shorts/', include('shorts_app.urls')),
 ]
 
+# This line below is what was causing the error
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
