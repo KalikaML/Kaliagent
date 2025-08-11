19064c4b89439f42baa11dc52574d6d3862eb050
Yuvraj2110068 <yuvrajput1537@gmail.com>
2025-08-11 14:14:52 +0530

    feat: Add URL routing for marketing outreach in the command center

--- AI_agent_center/command_center/urls.py
+++ AI_agent_center/command_center/urls.py
@@ -1,8 +1,7 @@
 # command_center/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings  # <-- ADD THIS LINE
+from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -10,8 +9,15 @@ urlpatterns = [
     path('', include('core.urls')),
     path('pitch/', include('ai_agent_pitch.urls')),
     path('shorts/', include('shorts_app.urls')),
+    
+    # --- ADD THESE TWO LINES ---
+    # This tells Django to look in marketing_outreach/urls.py for any URL starting with 'marketing/'
+    path('marketing/', include('marketing_outreach.urls')),
+    
+    # This tells Django to look in procurement/urls.py for any URL starting with 'procurement/'
+    path('procurement/', include('procurement.urls')),
 ]
 
-# This line below is what was causing the error
+# This is important for serving media files (like thumbnails) during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
